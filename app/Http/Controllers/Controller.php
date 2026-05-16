40cbb6e1ef65cb957c583e46181b92e75a79b327
Javier Villagran <javiervillagranplacencio@gmail.com>
2026-05-16 10:10:50 -0600

    feat: extend routing controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 
-abstract class Controller
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests; // Permite $this->authorize en los controladores
+    use ValidatesRequests; // Permite poner las rules en los controladores
 }
